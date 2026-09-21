e502cda1f55184d9acbc96fa92269d950f4273e3
payal priya <payalpriya8360@gmail.com>
2026-09-21 17:06:41 +0530

    Set basePath to '/admin' in next.config.mjs

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -22,6 +22,7 @@ if (process.cwd() !== projectRoot) {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export',
+  basePath: '/admin',
   outputFileTracingRoot: projectRoot,
   typescript: {
     ignoreBuildErrors: true,
